0b32a75e2c1c190b3cf29ba02ca8b9c455d3b96e
huahua <1494133104@qq.com>
2025-05-20 13:12:54 +0800

    feat: integrate vite-auto-i18n-plugin for multilingual support and add language files

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import '../lang/index.js';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginsAutoI18n, { 
+  // YoudaoTranslator, 
+  GoogleTranslator, 
+  // BaiduTranslator 
+} from 'vite-auto-i18n-plugin'
+
+const i18nPlugin = vitePluginsAutoI18n({
+  globalPath: './lang', // 存放翻译文件的目录
+  namespace: 'lang', // 命名空间
+  distPath: './dist/assets',
+  distKey: 'index',
+  targetLangList: ['en', 'ko', 'ja'], // 目标语言列表，英文，韩文，日文
+  originLang: 'zh-cn',
+  // 选择翻译器，有道、谷歌或百度
+  // 有道翻译
+  // translator: new YoudaoTranslator({
+  //     appId: '你的AppId',
+  //     appKey: '你的AppKey'
+  // })
+  // 谷歌翻译（需配置代理）
+  translator: new GoogleTranslator({
+    proxyOption: {
+        host: '127.0.0.1',
+        port: 13385,
+        headers: {
+            'User-Agent': 'Node'
+        }
+    }
+  })
+  // 百度翻译
+  // translator: new BaiduTranslator({
+  //   appId: '你的AppId',
+  //   appKey: '你的AppKey'
+  // })
+})
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    i18nPlugin
+  ],
 })
